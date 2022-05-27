065e1d1dcefdba9b9255f346cfc765566ccfcb7f
Nikola Kovacevic <nikola.kovacevic@sanofi.com>
2022-05-27 08:24:15 +0200

    feat: declares components

--- sample-vue/src/components/Home.vue
+++ sample-vue/src/components/Home.vue
@@ -8,7 +8,12 @@
       </b-col>
       <b-col class="dashboard-item" sm="4">
         <div class="card">
-          <Saturn />
+          <h3 class="card-title">Pressure (psi)</h3>
+          <vue-gauge
+            :refid="'type-unique-id'"
+            :options="gaugeOptions"
+          ></vue-gauge>
+          <h3 class="card-title central">5.3</h3>
         </div>
       </b-col>
     </b-row>
@@ -36,6 +41,18 @@
 <script>
 export default {
   name: "home",
+  data() {
+    return {
+      gaugeOptions: {
+        outerNeedle: true,
+        needleColor: "white",
+        needleValue: 85,
+        arcDelimiters: [40, 60],
+        arcColors: ["rgb(255,84,84)", "rgb(239,214,19)", "rgb(61,204,91)"],
+        rangeLabel: [],
+      },
+    };
+  },
 };
 </script>
 <style lang="scss">
@@ -46,12 +63,33 @@ div.py-3 {
       padding: 20px;
 
       .card {
-        border: 1px solid black;
+        border: 1px solid #111826;
         border-radius: 25px;
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
           0 6px 20px 0 rgba(0, 0, 0, 0.19);
         overflow: hidden;
+        min-height: 100%;
+        // background: #040c24;
+        background-image: linear-gradient(
+          99.9deg,
+          rgba(3, 9, 23, 1) 21.2%,
+          rgba(8, 21, 37, 1) 84.8%
+        );
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        .card-title {
+          color: #eee;
+          text-transform: uppercase;
+          font-weight: bold;
+        }
       }
+    }
+    .central {
+      position: absolute;
+      top: 55%;
     }
   }
 }
